b0e0831665a9a387c61d5afe93504126be9bf6b8
Jonas M Luster <j@d8c.us>
2010-10-12 13:44:58 -0500

    more fonts

--- r_Media.lua
+++ r_Media.lua
@@ -1,5 +1,9 @@
-local LSM = LibStub("LibSharedMedia-3.0")
+local l = LibStub("LibSharedMedia-3.0")
 local p = [[Interface\Addons\r_Media]]
 
-LSM:Register("font", "36 days ago BRK", p..[[\f\36daysag.ttf]])
-LSM:Register("font", "Good Times", p..[[\f\GOODTIME.ttf]])
+l:Register("font", "36 days ago BRK", p..[[\f\36daysag.ttf]])
+l:Register("font", "Good Times", p..[[\f\GOODTIME.ttf]])
+l:Register("font", "ElroNet Monospace", p..[[\f\elronmonospace.ttf]])
+l:Register("font", "LCDMono", p..[[\f\LCDmono.ttf]])
+l:Register("font", "Secret Code", p..[[\f\sectretcode.ttf]])
+l:Register("font", "ModeNine", p..[[\f\modenine.ttf]])
