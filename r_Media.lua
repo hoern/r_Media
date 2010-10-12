1779d814195968602834d61c879c7b4eb6369d45
Jonas M Luster <j@d8c.us>
2010-10-12 13:30:34 -0500

    files

---
+++ r_Media.lua
@@ -0,0 +1,5 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+local p = [[Interface\Addons\r_Media]]
+
+LSM:Register("font", "36 days ago BRK", p..[[\f\36daysag.ttf]])
+LSM:Register("font", "Good Times", p..[[\f\GOODTIME.ttf]])
